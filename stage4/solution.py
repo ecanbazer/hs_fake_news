119c3ed608715692adf03901037c25cffa2daaeb
ecanbazer <71968031+ecanbazer@users.noreply.github.com>
2023-07-12 14:36:22 +0300

    adding checkpoint

--- stage4/solution.py
+++ stage4/solution.py
@@ -4,8 +4,10 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+
 
 
 df = pd.read_csv('../fake_or_real_news.csv')
@@ -28,17 +30,19 @@ x_train, x_test, y_train, y_test = train_test_split(data, encoded_labels, test_s
 # Step 6: Build the model
 model = Sequential()
 model.add(Embedding(input_dim=1000, output_dim=128, input_length=max_sequence_length))
-model.add(LSTM(128, return_sequences=True,dropout=0.2))  
+model.add(LSTM(128, return_sequences=True))  
 model.add(LSTM(32))
 model.add(Dense(1, activation='sigmoid'))
+
+checkpoint_filepath = 'best_model.hdf5'
+model_save = ModelCheckpoint(checkpoint_filepath , save_best_only=True)
 
 # Step 7: Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Step 8: Train the model
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=32)
-
-# Step 9: Evaluate the model
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=32, callbacks = [model_save])
+model.load_weights(checkpoint_filepath)
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f'Test loss: {loss}')
 print(f'Test accuracy: {accuracy}')
