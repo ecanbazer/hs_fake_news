c89dc822b3eaed35e77c0ac2c748a7d4c6b89b8d
ecanbazer <71968031+ecanbazer@users.noreply.github.com>
2023-07-19 16:21:03 +0300

    final

--- stage4/solution.py
+++ stage4/solution.py
@@ -1,13 +1,15 @@
-import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, LSTM, Dropout
+from tensorflow.keras.layers import Dense, Embedding, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import AdamW
+from tensorflow.keras.utils import set_random_seed
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+
+set_random_seed(41)
 
 df = pd.read_csv('../fake_or_real_news.csv')
 X = df['text']
@@ -28,14 +30,13 @@ x_train, x_test, y_train, y_test = train_test_split(data, encoded_labels, test_s
 model = Sequential()
 model.add(Embedding(input_dim= num_words, output_dim=100, input_length=max_sequence_length))
 model.add(LSTM(100))
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
 checkpoint_filepath = 'best_model.hdf5'
 model_save = ModelCheckpoint(checkpoint_filepath , save_best_only=True,  monitor='val_accuracy')
 
 model.compile(optimizer= AdamW(), loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, validation_split = 0.1, epochs=15, batch_size=32, callbacks = [model_save])
+model.fit(x_train, y_train, validation_split = 0.1, epochs=10, batch_size=32, callbacks = [model_save])
 model.load_weights(checkpoint_filepath)
 loss, accuracy = model.evaluate(x_test, y_test)
-print(f'Test loss: {loss}')
 print(f'Test accuracy: {accuracy}')
